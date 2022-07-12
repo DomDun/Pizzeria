bc52644872fcc2dbcbea0ff0895e018c713e4905
Dominik <ddunst7@gmail.com>
2022-07-12 18:34:03 +0200

    [task] few updates

--- PizzeriaDoublePineapple/PizzeriaDoublePineapple.Data/OrdersRepository.cs
+++ PizzeriaDoublePineapple/PizzeriaDoublePineapple.Data/OrdersRepository.cs
@@ -11,33 +11,33 @@ namespace PizzeriaDoublePineapple.Data
         private string _connectionString = ConfigurationManager.ConnectionStrings["PizzeriaDoublePineappleDbConnectionString"].ConnectionString;
         public void AddOrder(OrderData order)
         {
-            //SqlTransaction transaction;
+            SqlTransaction transaction;
 
             try
             {
                 using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
                     connection.Open();
-                    //transaction = connection.BeginTransaction();
+                    transaction = connection.BeginTransaction();
 
                     string insertOrderCommandText = $"INSERT INTO [Orders] ([PhoneNumber], [Date], [TotalCost]) VALUES (@PhoneNumber, @Date, @TotalCost)";
 
-                    SqlCommand insertOrderCommand = new SqlCommand(insertOrderCommandText, connection/*, transaction*/);
+                    SqlCommand insertOrderCommand = new SqlCommand(insertOrderCommandText, connection, transaction);
                     insertOrderCommand.Parameters.Add("@PhoneNumber", SqlDbType.NVarChar, 255).Value = order.ClientData.PhoneNumber;
                     insertOrderCommand.Parameters.Add("@Date", SqlDbType.DateTime2).Value = order.OrderDate;
                     insertOrderCommand.Parameters.Add("@TotalCost", SqlDbType.Float, 8).Value = order.TotalCost;
                     insertOrderCommand.ExecuteNonQuery();
 
-                    string getLastOrderIdCommandText = $"SELECT IDENT_CURRENT ('Orders') AS [OrderId]";
-                    SqlCommand getLastOrderIdCommand = new SqlCommand(getLastOrderIdCommandText, connection/*, transaction*/);
+                    string getLastOrderIdCommandText = $"SELECT max(id) From Orders";
+                    SqlCommand getLastOrderIdCommand = new SqlCommand(getLastOrderIdCommandText, connection, transaction);
                     SqlDataReader sqlDataReader = getLastOrderIdCommand.ExecuteReader();
 
                     sqlDataReader.Read();
-                    int orderId = int.Parse(sqlDataReader["OrderId"].ToString());
+                    int orderId = int.Parse(sqlDataReader[0].ToString());
                     sqlDataReader.Close();
 
                     string insertOrdersCommandText = $"INSERT INTO [PizzasOrders] ([OrderId], [PizzaId], [PizzaName], [PriceS], [PriceM], [PriceL], [PizzaSize]) VALUES (@OrderId, @PizzaId, @PizzaName, @PriceS, @PriceM, @PriceL, @PizzaSize)";
-                    SqlCommand insertPizzaOrdersCommand = new SqlCommand(insertOrdersCommandText, connection/*, transaction*/);
+                    SqlCommand insertPizzaOrdersCommand = new SqlCommand(insertOrdersCommandText, connection, transaction);
                     foreach (PizzaData item in order.Pizzas)
                     {
                         insertPizzaOrdersCommand.Parameters.Add("@OrderId", SqlDbType.Int).Value = orderId;
@@ -51,7 +51,7 @@ namespace PizzeriaDoublePineapple.Data
                     }
 
                     string insertSauceOrdersCommandText = $"INSERT INTO [SaucesOrders] ([OrderId], [SauceId], [SauceName], [Price]) VALUES (@OrderId, @SauceId, @SauceName, @Price)";
-                    SqlCommand insertSauceOrdersCommand = new SqlCommand(insertOrdersCommandText, connection/*, transaction*/);
+                    SqlCommand insertSauceOrdersCommand = new SqlCommand(insertOrdersCommandText, connection, transaction);
                     foreach (SauceData sauce in order.Sauces)
                     {
                         insertSauceOrdersCommand.Parameters.Add("@OrderId", SqlDbType.Int).Value = orderId;
@@ -63,11 +63,11 @@ namespace PizzeriaDoublePineapple.Data
 
                     // todo: zrobić update dateOfLastOrder   string changeLastDateOfOrder = $"UPDATE [Clients] SET [DateOfLastOrder] VALUES @Date WHERE [Orders][PhoneNumber] = @PhoneNumber";
 
-                    //transaction.Commit();
+                    transaction.Commit();
                     connection.Close();
                 }
+                //todo: dodawanie do sqla się wysypuje, sprawdzić to.
             }
-            // todo: zrobić zapisywanie do sqla.
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
